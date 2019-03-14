de204a61773eebaead03a3fd58dbe65c8aae32f3
nav <wanglonghui@aerozhonghuan.com>
2019-03-14 17:14:00 +0800

    init library

---
+++ android/src/main/java/com/aero/flashlight/RNCarrotFlashlightModule.java
@@ -0,0 +1,22 @@
+
+package com.aero.flashlight;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNCarrotFlashlightModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNCarrotFlashlightModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNCarrotFlashlight";
+  }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNCarrotFlashlight } = NativeModules;
+
+export default RNCarrotFlashlight;
