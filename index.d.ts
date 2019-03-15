21b1f87477945ecdd0766c6583b4a828b6645478
nav <wanglonghui@aerozhonghuan.com>
2019-03-15 17:51:32 +0800

    fix setFlashlight

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module "react-native-carrot-flashlight" {
     declare const CarrotFlashlight: {
-        setFlashlight(state:boolean,callback?:()=>void) => void;
+        setFlashlight(state:boolean,callback?:()=>void) : void;
     }
     export default CarrotFlashlight;
 }
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 import { NativeModules } from 'react-native';
 
 const { RNCarrotFlashlight } = NativeModules;
 
 export default {
-    setFlashlight() {
-        return RNCarrotFlashlight.setFlashlight;
+    setFlashlight(isOpen, callback) {
+        return RNCarrotFlashlight.setFlashlight(isOpen,callback);
     },
 };
