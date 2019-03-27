a8bcc4aa2968e134d665a8c9419bfaf48ff05ab5
nav <wanglonghui@aerozhonghuan.com>
2019-03-27 15:52:31 +0800

    Perfect method parameters

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,20 @@
+/*
+ * @Description: react-native-carrot-flashlight,Solve the problem of launching a white screen in a React Native project
+ * @Author: wanglh
+ * @LastEditors: wanglh
+ * @Date: 2018-05-29 14:55:15
+ * @LastEditTime: 2019-03-27 15:50:59
+ */
+
 declare module "react-native-carrot-flashlight" {
     declare const CarrotFlashlight: {
-        setFlashlight(state:boolean,callback?:()=>void) : void;
+        /**
+         * @description: Turn the flashlight on or off
+         * @param isOpen {boolean} true:on, false:off
+         * @param callback {func}  isSucc: true or false, errMsg: error desc
+         * @return: void
+         */
+        setFlashlight(isOpen:boolean,callback?:(isSucc:boolean,errMsg:string)=>void) : void;
     }
     export default CarrotFlashlight;
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/*
+ * @Description: react-native-carrot-flashlight,Solve the problem of launching a white screen in a React Native project
+ * @Author: wanglh
+ * @LastEditors: wanglh
+ * @Date: 2018-05-29 14:55:15
+ * @LastEditTime: 2019-03-27 15:51:13
+ */
 import { NativeModules } from 'react-native';
 
 const { RNCarrotFlashlight } = NativeModules;
 
-export default {
-    setFlashlight(isOpen, callback) {
-        return RNCarrotFlashlight.setFlashlight(isOpen, callback);
-    },
-};
+export default RNCarrotFlashlight;
